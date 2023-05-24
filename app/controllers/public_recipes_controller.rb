788c12ebc439692483a7391614c5ee125c2bd9ac
EmekaEmmanuel <emekaekeohaa@gmail.com>
2023-05-24 02:26:43 -0700

    Add login and registeration devise

---
+++ app/controllers/public_recipes_controller.rb
@@ -0,0 +1,13 @@
+class PublicRecipesController < ApplicationController
+  def index
+    @recipes = Recipe.where(public: true)
+    @price = find_price
+  end
+
+  def find_price
+    price = 0
+    recipe = Recipe.first
+    recipe.foods.map { |rec| price += rec.price }
+    price
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,26 @@
 Rails.application.routes.draw do
+  devise_for :users
+
+   # Commented to prevent defaulting to sign in view to check if signed in or not
+    devise_scope :user do
+      root 'devise/sessions#new'
+    end
+
+  resources :recipes
+  
+  resources :recipe_foods
+
+  resources :public_recipes
+
+  resources :foods, except: [:update]
+
+  resources :recipes, only: [:index, :show, :new, :create, :destroy] do
+    resources :recipe_foods, only: [:new, :create, :destroy, :update, :edit]
+  end
+
+  resources :users, only: [:index, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  
 end
